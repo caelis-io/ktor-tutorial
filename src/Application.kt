0df520f7b64a7f48c7ed08fff48533004a0cbf10
Max <coding@maax.gr>
2020-06-07 11:15:30 +0200

    initial commit

---
+++ src/Application.kt
@@ -0,0 +1,34 @@
+package me.caelis
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    val client = HttpClient(Apache) {
+
+        routing {
+
+            get("/") {
+                call.respondText("Hello Ktor!")
+            }
+
+            get("/test") {
+                call.respondText("This is a test")
+            }
+
+        }
+
+    }
+
+}
+
